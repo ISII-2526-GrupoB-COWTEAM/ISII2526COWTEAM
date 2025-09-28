656205785909335d0449e977416756ec3019d1ae
jaimerodriguez7 <jaime.rodriguez7@alu.uclm.es>
2025-09-28 20:06:46 +0200

    Clase PurchaseItem creada y arreglo error de dependencias con Purchase

--- src/AppForSEII2526.API/Models/Purchase.cs
+++ src/AppForSEII2526.API/Models/Purchase.cs
@@ -2,14 +2,14 @@
 {
     public class Purchase
     {
-        String CustomerUserName { get; set; }
-        String CustomerSurname { get; set; }
-        String DeliveryAddress { get; set; }
-        int Id { get; set; }
-        PaymentMethod paymentMethod { get; set; }
-        DateTime PurchaseDate { get; set; }
-        double TotalPrice { get; set; }
-        int TotalQuantity { get; set; }
+        public String CustomerUserName { get; set; }
+        public String CustomerSurname { get; set; }
+        public String DeliveryAddress { get; set; }
+        public int Id { get; set; }
+        public PaymentMethod paymentMethod { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public double TotalPrice { get; set; }
+        public int TotalQuantity { get; set; }
 
         public override bool Equals(object? obj)
         {
