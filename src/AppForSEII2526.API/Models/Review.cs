6390b9cbda042627373894e6a49df48b6664d6e4
Pablo Sanchez Martinez <Pablo.Sanchez29@alu.uclm.es>
2025-10-05 00:41:10 +0200

    Se han hecho algunas correcciones en la clase Review, pero queda pendiente de revisión en tutorías porque hay muchas incongruencias en el modelo del diagrama de clases que se ha proporcionado y lo que se explica en las clases en relación a ApplicationUser :).

--- src/AppForSEII2526.API/Models/Review.cs
+++ src/AppForSEII2526.API/Models/Review.cs
@@ -4,48 +4,40 @@
 
     public class Review
     {
-        //Falta implementar el atributo CustomerCount... que aparece truncado
-        //En la imagen del diagrama de clases para este caso de uso.
         public Review()
         {
         }
 
-        public Review(int customerId, DateTime dateOfReview, int overallRating, string reviewTitle, IList<ReviewItem> reviewItems)
+        public Review(/*ApplicationUser applicationUser,*/ int customerId, DateTime dateOfReview, int rating, string reviewTitle, IList<ReviewItem> reviewItems)
         {
-            //CustomerCount...
-            CustomerId = customerId;
-            OverallRating = (reviewItems != null && reviewItems.Count > 0)
-                ? (int)reviewItems.Average(ri => ri.Rating)
-                : overallRating;
+            //Preguntar en tutorias, entiendo que el customerId en verdad es el id de applicationUser
+            //ApplicationUser = applicationUser;
 
+            CustomerId = customerId;
+            Rating = rating;
             DateOfReview = dateOfReview;
             ReviewTitle = reviewTitle;
-            ReviewItems = reviewItems ?? new List<ReviewItem>();
-        }
-
-        public Review(int customerId, DateTime dateOfReview, int overallRating, string reviewTitle)
-        {
-            //CustomerCount...
-            CustomerId = customerId;
-            OverallRating = overallRating;
-            DateOfReview = dateOfReview;
-            ReviewTitle = reviewTitle;
-            ReviewItems = new List<ReviewItem>();
+            ReviewItems = reviewItems ?? new List<ReviewItem>(); 
+            
+            //Preguntar en tutorias
+            /*ReviewItems = reviewItems != null 
+                ? new List<ReviewItem>(reviewItems)
+                : new List<ReviewItem>();
+            */
         }
 
         public int ReviewId { get; set; }
 
-        //Restriccion
-        //public ??? CustomerCount... { get; set; }
-
+        //Preguntar tutorias, creo que tienen la misma funcion
         public int CustomerId { get; set; }
+        //public ApplicationUser ApplicationUser { get; set; }
 
         [DataType(DataType.Date)]
         [Display(Name = "Date Of Review")]
         public DateTime DateOfReview { get; set; }
 
         [Range(1, 5, ErrorMessage = "Overall rating must be between 1 and 5")]
-        public int OverallRating { get; set; }
+        public int Rating { get; set; }
 
         [StringLength(100, ErrorMessage = "Review title cannot be longer than 100 characters.")]
         public string ReviewTitle { get; set; }
