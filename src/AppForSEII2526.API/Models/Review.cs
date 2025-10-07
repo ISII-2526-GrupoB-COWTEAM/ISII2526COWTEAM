1d920506aac7681bcf6be5eb95a87a72ab8f8670
Pablo Sanchez Martinez <Pablo.Sanchez29@alu.uclm.es>
2025-10-07 13:34:26 +0200

    Cambios en la clase Review tras la tutoría

--- src/AppForSEII2526.API/Models/Review.cs
+++ src/AppForSEII2526.API/Models/Review.cs
@@ -8,41 +8,29 @@
         {
         }
 
-        public Review(/*ApplicationUser applicationUser,*/ int customerId, DateTime dateOfReview, int overallRating, string reviewTitle, IList<ReviewItem> reviewItems)
+        public Review(ApplicationUser applicationUser, DateTime dateOfReview, string reviewTitle, IList<ReviewItem> reviewItems)
         {
-            //Preguntar en tutorias, entiendo que el customerId en verdad es el id de applicationUser
-            //ApplicationUser = applicationUser;
-
-            CustomerId = customerId;
+            ApplicationUser = applicationUser;
 
             //Preguntar en tutorias, que es este atributo? se recibe por parametro o es calculado?
             OverallRating = (reviewItems != null && reviewItems.Count > 0)
                 ? (int)reviewItems.Average(ri => ri.Rating)
-                : overallRating;
-
-
+                : 0;
             DateOfReview = dateOfReview;
             ReviewTitle = reviewTitle;
-            ReviewItems = reviewItems ?? new List<ReviewItem>(); 
-            
-            //Preguntar en tutorias
-            /*ReviewItems = reviewItems != null 
+            ReviewItems = reviewItems != null 
                 ? new List<ReviewItem>(reviewItems)
                 : new List<ReviewItem>();
-            */
+           
         }
 
         public int ReviewId { get; set; }
-
-        //Preguntar tutorias, creo que tienen la misma funcion
-        public int CustomerId { get; set; }
-        //public ApplicationUser ApplicationUser { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
 
         [DataType(DataType.Date)]
         [Display(Name = "Date Of Review")]
         public DateTime DateOfReview { get; set; }
 
-        [Range(1, 5, ErrorMessage = "Overall rating must be between 1 and 5")]
         public int OverallRating { get; set; }
 
         [StringLength(100, ErrorMessage = "Review title cannot be longer than 100 characters.")]
