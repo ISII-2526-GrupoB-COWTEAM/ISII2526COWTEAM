0fbca88bbe251b314755dff078f83f82db7bbdad
Carlos Sánchez Martínez <carlos.sanchez68@alu.uclm.es>
2025-10-17 14:19:52 +0200

    Creado el DTO y el controller de repair

---
+++ src/AppForSEII2526.API/Controllers/RepairController.cs
@@ -0,0 +1,42 @@
+﻿using AppForSEII2526.API.DTOs.RepairDTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RepairController : ControllerBase
+    {
+
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<RepairController> _logger;
+
+        public RepairController(ApplicationDbContext context, ILogger<RepairController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<RepairDTO>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> GetRepairsDTO(decimal op1, decimal op2)
+        {
+
+            var repairs = await _context.Repair.
+                Select(r => new RepairDTO(
+                    r.Id,
+                    r.Name,
+                    r.Scale.Name,
+                    r.Cost,
+                    r.Description
+                )).
+                ToListAsync();
+
+            return Ok(repairs);
+
+        }
+    }
+}
--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -5,4 +5,8 @@ using AppForSEII2526.API.Models;
 namespace AppForSEII2526.API.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
+    public DbSet<Scale> Scale { get; set; }
+    public DbSet<Repair> Repair { get; set; }
+    public DbSet<Receipt> Receipt { get; set; }
+    public DbSet<ReceiptItem> ReceiptItem { get; set; }
 }
