3c20d776d9cec8b7d55342f5861bccfa21746ab3
CARLOS SÁNCHEZ MARTÍNEZ <Carlos.Sanchez68@alu.uclm.es>
2025-10-21 19:39:15 +0200

    Correccion del conflicto en APPDBContext

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -9,4 +9,10 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
     public DbSet<Repair> Repair { get; set; }
     public DbSet<Receipt> Receipt { get; set; }
     public DbSet<ReceiptItem> ReceiptItem { get; set; }
+
+    public DbSet<Device> Device { get; set; }
+    public DbSet<Model> Model { get; set; }
+    public DbSet<ReviewItem> ReviewItem { get; set; }
+    public DbSet<Review> Review { get; set; }
+
 }
