4bbc7b127fcb1b875190210644ed56f31230303e
Pablo Sanchez Martinez <Pablo.Sanchez29@alu.uclm.es>
2025-10-21 19:19:14 +0200

    Creada la clase ReviewsController y añadida la carpeta de DTOs para las Reviews.
    No se ha añadido código.

---
+++ src/AppForSEII2526.API/Controllers/ReviewsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<DevicesController> _logger;
+
+        public ReviewsController(ApplicationDbContext context, ILogger<DevicesController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+    }
+}
