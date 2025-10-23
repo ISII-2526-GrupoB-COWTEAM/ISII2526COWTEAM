2d501759f40ec2d69ed23c67fa000cdf853b33bb
Pablo Sanchez Martinez <Pablo.Sanchez29@alu.uclm.es>
2025-10-23 09:55:27 +0200

    Esqueleto del GET de detalle, faltan aun cosas y DTOs.

--- src/AppForSEII2526.API/Controllers/ReviewsController.cs
+++ src/AppForSEII2526.API/Controllers/ReviewsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AppForSEII2526.API.DTOs.DeviceDTO;
+using AppForSEII2526.API.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AppForSEII2526.API.Controllers
@@ -14,6 +16,29 @@ namespace AppForSEII2526.API.Controllers
         {
             _context = context;
             _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<ReviewDetailDTO>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> GetReview(int id)
+        {
+            var review = await _context.Review
+                .Where(r => r.Id == id)
+                    .Include(r => r.ReviewItems)
+                        .ThenInclude(ri => ri.Device)
+                            .ThenInclude(device => device.Model)
+                .Select(d => new ReviewDetailDTO())
+
+            .FirstOrDefaultAsync();
+
+            if (review == null)
+            {
+                _logger.LogError($"Error: Review with id {id} does not exist");
+                return NotFound();
+            }
+
+            return Ok(review);
         }
 
     }
