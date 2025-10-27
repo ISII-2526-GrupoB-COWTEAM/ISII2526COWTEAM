18ae67239ffd131244a82fae452047fbdba23d88
CARLOS SÁNCHEZ MARTÍNEZ <Carlos.Sanchez68@alu.uclm.es>
2025-10-27 19:19:35 +0100

    Hecho todo lo relativo a GetDetail

---
+++ src/AppForSEII2526.API/DTOs/ReceiptDTO/ReceiptForCreate.cs
@@ -0,0 +1,36 @@
+﻿using AppForSEII2526.API.DTOs.ReviewDTO;
+using AppForSEII2526.API.Models;
+
+namespace AppForSEII2526.API.DTOs.ReceiptDTO
+{
+    public class ReceiptForCreate
+    {
+
+        public ReceiptForCreate(string applicationUserName, string applicationUserSurname,
+            string deliveryAddress, IList<ReceiptItemDTO> receiptItems)
+        {
+            ApplicationUserName = applicationUserName ?? throw new ArgumentNullException(nameof(applicationUserName));
+            ApplicationUserSurname = applicationUserSurname ?? throw new ArgumentNullException(nameof(applicationUserSurname));
+            DeliveryAddress = deliveryAddress ?? throw new ArgumentNullException(nameof(deliveryAddress));
+            ReceiptItems = receiptItems ?? throw new ArgumentNullException(nameof(receiptItems));
+        }
+        [Required]
+        public string ApplicationUserName { get; set; }
+        
+        [Required]
+        public string ApplicationUserSurname { get; set; }
+
+        [Required]
+        public string DeliveryAddress { get; set; }
+
+        [Required]
+        public IList<ReceiptItemDTO> ReceiptItems { get; set; }
+
+
+        public ReceiptForCreate()
+        {
+            ReceiptItems = new List<ReceiptItemDTO>();
+        }
+
+    }
+}
---
+++ src/AppForSEII2526.API/DTOs/ReceiptDTO/ReceiptItemDTO.cs
@@ -0,0 +1,24 @@
+﻿namespace AppForSEII2526.API.DTOs.ReceiptDTO
+{
+    public class ReceiptItemDTO
+    {
+        public ReceiptItemDTO(int repairID, string name, string scale, double cost, string model)
+        {
+            RepairID = repairID;
+            Name = name;
+            Scale = scale;
+            Cost = cost;
+            Model = model;
+        }
+
+        public int RepairID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Scale { get; set; }
+        [Required]
+        public double Cost { get; set; }
+        [Required]
+        public string Model { get; set; }
+    }
+}
