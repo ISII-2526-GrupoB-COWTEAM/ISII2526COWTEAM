c23535bb1ffbadd6daa404d13cad096d647b7791
jaimerodriguez7 <jaime.rodriguez7@alu.uclm.es>
2025-10-28 08:50:32 +0100

    Creacion devices controller for purchase

---
+++ src/AppForSEII2526.API/Controllers/DevicesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AppForSEII2526.API.DTOs;
+using AppForSEII2526.API.DTOs.PurchaseDTO;
+
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DevicesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<DevicesController> _logger;
+
+        public DevicesController(ApplicationDbContext context, ILogger<DevicesController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<DeviceForPurchaseDTO>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetDevicesForPurchase()
+        {
+            var devices = await _context.Device
+                .Select(d => new DeviceForPurchaseDTO
+                (
+                    d.Id,d.Name,d.Color,d.PriceForPurchase, d.Model.NameModel,d.Brand
+                ))
+                .ToListAsync();
+            return Ok(devices);
+
+        }
+    }
+}
--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -5,4 +5,8 @@ using AppForSEII2526.API.Models;
 namespace AppForSEII2526.API.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
-}
+    public DbSet<Device> Device { get; set; }
+    public DbSet<Model> Model { get; set; }
+    public DbSet<Purchase> Purchase { get; set; }
+    public DbSet<PurchaseItem> PurchaseItem { get; set; }
+    }
